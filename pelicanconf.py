44dc1f9cd82072207547b1e1ce395d445b58f4e4
Andy <AndySuen@users.noreply.github.com>
2019-02-20 18:23:10 +0800

    init pelican

---
+++ pelicanconf.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Andy'
+SITENAME = "Andy's blog"
+SITEURL = 'http://localhost:8000'
+
+TWITTER_USERNAME = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Hong_Kong'
+DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/ibingfei'),
+          ('github', 'https://github.com/andysuen'),)
+
+DEFAULT_PAGINATION = 10
+
+STATIC_PATHS = ['images']
+
+PAGE_DIRS = ['pages']
+ARTICLE_DIRS = ['articles']
+
+ARTICLE_URL = 'blog/{date:%Y}/{slug}'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{slug}/index.html'
+
+SIDEBAR_DIGEST = 'Web developer'
+
+SHOW_ARCHIVES = True
+SHOW_FEED = False  # Need to address large feeds
+
+MENUITEMS = [("Home", "/"), ("Archive", "/archives.html")]
+
+DISPLAY_PAGES_ON_MENU = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
+
+THEME = 'pelican-themes/pelican-blue'
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
